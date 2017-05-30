4aab3799bb88e5f326385f8d3da7242dc3aa4cfe
soumya-kalluri1 <soumya.kalluri1@gmail.com>
2017-05-30 11:10:34 -0700

    Added code to use GPS and Network instead of setMyLocationEnabled

--- MyGoogleMaps/app/src/main/java/com/example/soumyakalluri/mygooglemaps/MapsActivity.java
+++ MyGoogleMaps/app/src/main/java/com/example/soumyakalluri/mygooglemaps/MapsActivity.java
@@ -2,11 +2,15 @@ package com.example.soumyakalluri.mygooglemaps;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -23,6 +27,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleApiClient mGoogleApiClient;
     public static final int REQUEST_FINE = 2;
     public static final int REQUEST_COARSE = 2;
+
+    private LocationManager locationManager;
+    private boolean isGPSenabled = false;
+    private boolean isNetworkEnabled = false;
+    private boolean canGetLocation = false;
+
+    private static final long MIN_TIME_BTWN_UPDATES = 1000*15;
+    private static final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 5.0f;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,5 +95,105 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void setTerrainView(View v)
     {
         if (mMap.getMapType()!=3) mMap.setMapType(3);
+    }
+
+    public void getLocation(View v)
+    {
+        try
+        {
+            locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+            // Get GPS status
+            isGPSenabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            if (isGPSenabled) Log.d("MyMapsApp","getLocation: GPS is enabled");
+
+            // Get network status
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (isNetworkEnabled) Log.d("MyMapsApp", "getLocation: network is enabled");
+
+            if (!(isGPSenabled && isNetworkEnabled))
+            {
+                Log.d("MyMapsApp","getLocation: No Provider is enabled");
+            }
+            else
+            {
+                canGetLocation = true;
+                if (isGPSenabled)
+                {
+                    Log.d("MyMapsApp", "getLocation: GPS is enabled - requesting location updates");
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            MIN_TIME_BTWN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerGPS);
+                    Log.d("MyMapsApp","getLocation: Network GPS update request success");
+                    Toast.makeText(this, "Using GPS", Toast.LENGTH_SHORT);
+                }
+
+                if (isNetworkEnabled)
+                {
+                    Log.d("MyMapsApp","getLocation: Network is enabled - requesting location updates");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BTWN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerNetwork);
+                    Log.d("MyMapsApp","getLocation: Network update request success");
+                    Toast.makeText(this, "Using Network", Toast.LENGTH_SHORT);
+                }
+                Log.d("MyMapsApp","getLocation: Can get location");
+            }
+
+        }
+        catch (Exception e)
+        {
+            Log.d("MyMapsApp", "getLocation: Caught an exception in getLocation");
+            e.printStackTrace();
+        }
+    }
+
+    public class locationListenerGPS implements LocationListener
+    {
+        @Override
+        public void onLocationChanged(Location location) {
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+            double time = location.getTime();
+            
+            Log.d("MyMapsApp","locationListenerGPS: Location has changed");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+
+    public class locationListenerNetwork implements LocationListener
+    {
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
     }
 }
